4e8abdbe55ef732318228af53a2cb06a4c0ac6b4
Elena Sviridova <valeryk13@yandex.ru>
2021-07-12 17:07:29 +0300

    cors13

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,16 +8,17 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const requestHeaders = 'content-type, application/json';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
 
   // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', '*');
-  }
+  // if (allowedCors.includes(origin)) {
+  //   // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
