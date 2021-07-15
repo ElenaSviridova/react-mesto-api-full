20b60cdc6e83bbae0d11a22a1406ccc4907bffed
Elena Sviridova <valeryk13@yandex.ru>
2021-07-15 12:50:24 +0300

    env2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,8 @@
-// import { JWT_SECRET } from '../controllers/user';
-
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET = 'some-secret-key' } = process.env;
-
 const NoAuthorizationError = require('../errors/no-authorization');
+
+const { JWT_SECRET = 'some-secret-key' } = process.env;
 
 module.exports = (req, res, next) => {
   // достаём авторизационный заголовок
