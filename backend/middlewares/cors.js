b68dfcdb6081bbad7d0ba13a8a094724716b8441
Elena Sviridova <valeryk13@yandex.ru>
2021-07-15 15:52:05 +0300

    env10 password4lettersOnly corsAll

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,18 +6,19 @@ const allowedCors = [
 ];
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
 
   // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  // res.header('Access-Control-Allow-Origin', '*');
+  // if (allowedCors.includes(origin)) {
+  //   // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+  //   res.header('Access-Control-Allow-Origin', origin);
+  //   res.header('Access-Control-Allow-Credentials', true);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credential', true);
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
