f0029bb24475b1b3cdde1260d9deeb2c48cad9a6
Elena Sviridova <valeryk13@yandex.ru>
2021-07-15 12:43:32 +0300

    env

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,14 +6,14 @@ const NotFoundError = require('../errors/not-found-error');
 const BadRequestError = require('../errors/bad-request-error');
 const ConflictError = require('../errors/conflict-error');
 
-const { JWT_SECRET = 'some-secret-key' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = {
   login(req, res, next) {
     const { email, password } = req.body;
     User.findUserByCredentials(email, password)
       .then((user) => {
-        const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+        const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
         return res.send({ token });
       })
       .catch(next);
