67b3ed71ec726f421587f55122bb9fe8dd9bcd39
Elena Sviridova <valeryk13@yandex.ru>
2021-07-15 15:42:24 +0300

    env10

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-require('dotenv').config();
+// require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
