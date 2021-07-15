5b40428df6bdadddd1643d1fd552949d45df406f
Elena Sviridova <valeryk13@yandex.ru>
2021-07-15 13:11:22 +0300

    env5

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -15,9 +15,9 @@ const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
-// console.log(process.env.NODE_ENV);
-// console.log(process.env.PORT);
-// console.log(process.env.JWT_SECRET);
+console.log(process.env.NODE_ENV);
+console.log(process.env.PORT);
+console.log(process.env.JWT_SECRET);
 
 const app = express();
 
